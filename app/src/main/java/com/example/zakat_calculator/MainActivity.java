9df42fa852030832d63d4df0e3b5903f7cba3544
adan35 <adanshahzad35@gmail.com>
2021-11-19 09:10:20 +0500

    adding TextView for showing Zakat on users amount

--- app/src/main/java/com/example/zakat_calculator/MainActivity.java
+++ app/src/main/java/com/example/zakat_calculator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         EditText editText = (EditText) findViewById(R.id.editTextNumber);
         Button btn = (Button) findViewById(R.id.button);
+        TextView zakat = (TextView) findViewById(R.id.textView4);
     }
 }
