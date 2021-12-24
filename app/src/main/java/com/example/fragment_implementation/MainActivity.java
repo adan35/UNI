7af85d1bcd35d4b94a39565343999f0fee23b6c8
adan35 <adanshahzad35@gmail.com>
2021-12-24 22:43:00 +0500

    implementation of main logic with java code

--- app/src/main/java/com/example/fragment_implementation/MainActivity.java
+++ app/src/main/java/com/example/fragment_implementation/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.fragment_implementation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragmentButton, secondFragmentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstFragmentButton = findViewById(R.id.fragment1);
+        secondFragmentButton = findViewById(R.id.fragment2);
+
+        firstFragmentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                replaceFragment(new Fragment1());
+            }
+        });
+
+        secondFragmentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                replaceFragment(new Fragment2());
+            }
+        });
     }
+
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
